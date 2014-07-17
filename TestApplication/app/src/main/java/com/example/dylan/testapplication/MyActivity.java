f8d553fa0ff1592be3aa65f3469299dbe09f492f
Andrew <aewp73@mst.edu>
2014-07-16 19:43:13 -0500

    Ding Ding Round 2
    RANDOMLY PLUGGING IN EXAMPLE CODE IS FUN.

--- TestApplication/app/src/main/java/com/example/dylan/testapplication/MyActivity.java
+++ TestApplication/app/src/main/java/com/example/dylan/testapplication/MyActivity.java
@@ -10,6 +10,7 @@ import android.widget.TextView;
 import android.media.AudioTrack;
 import android.media.AudioManager;
 import android.media.AudioFormat;
+import java.util.Random;
 
 public class MyActivity extends Activity {
 
@@ -39,21 +40,40 @@ public class MyActivity extends Activity {
         return super.onOptionsItemSelected(item);
     }
 
+    boolean m_stop = false;
+    AudioTrack m_audioTrack;
+    Thread m_noiseThread;
+
     public void buttonOnCLick(View v) {
-        AudioTrack audioTrack = new AudioTrack(AudioManager.STREAM_MUSIC, 44100, AudioFormat.CHANNEL_OUT_STEREO, AudioFormat.ENCODING_PCM_16BIT, 44100, AudioTrack.MODE_STREAM);
+        m_stop = false;
 
-        byte[] music1 = "888888888888888888".getBytes();
+        /* 8000 bytes per second*/
+        m_audioTrack = new AudioTrack(AudioManager.STREAM_MUSIC, 8000, AudioFormat.CHANNEL_OUT_MONO,
+                AudioFormat.ENCODING_PCM_8BIT, 8000 /* 1 second buffer */,
+                AudioTrack.MODE_STREAM);
 
-        audioTrack.play();
-        for(int i = 0; i < 100; i++) {
-            audioTrack.write(music1, 0, music1.length);
-        }
+        m_audioTrack.play();
 
-        Button button = (Button) v;
-        button.setText("you bitch!");
 
-        TextView myTextView = (TextView) findViewById(R.id.textView);
-        myTextView.setText("You're a big ol' bitch!");
-        // look at these comments asdfasdfadsf
+        m_noiseThread = new Thread(m_noiseGenerator);
+        m_noiseThread.start();
     }
+
+    Runnable m_noiseGenerator = new Runnable()
+    {
+        public void run()
+        {
+            Thread.currentThread().setPriority(Thread.MIN_PRIORITY);
+
+            /* 8000 bytes per second, 1000 bytes = 125 ms */
+            byte [] noiseData = new byte[1000];
+            Random rnd = new Random();
+
+            while(!m_stop)
+            {
+                rnd.nextBytes(noiseData);
+                m_audioTrack.write(noiseData, 0, noiseData.length);
+            }
+        }
+    };
 }
