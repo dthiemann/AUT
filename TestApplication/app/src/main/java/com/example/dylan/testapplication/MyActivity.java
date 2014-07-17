fb4ae1dff378ccfff77ec0dc5c19a665e8695997
Dylan Thiemann <dylanthiemann@uiowa.edu>
2014-07-16 19:03:37 -0500

    commit

--- TestApplication/app/src/main/java/com/example/dylan/testapplication/MyActivity.java
+++ TestApplication/app/src/main/java/com/example/dylan/testapplication/MyActivity.java
@@ -62,5 +62,6 @@ public class MyActivity extends Activity {
 
         TextView myTextView = (TextView) findViewById(R.id.textView);
         myTextView.setText("You're a big ol' bitch!");
+        // look at these comments
     }
 }
